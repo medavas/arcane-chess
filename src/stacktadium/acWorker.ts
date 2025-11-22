5a6808c5e29018a14b5062d2302aef4228982837
Ryan Deyo <rdeyo1129@gmail.com>
2025-11-22 14:54:54 -0500

    rm ai images and stacktadium

--- src/stacktadium/acWorker.ts
+++
@@ -1,9 +0,0 @@
-/// <reference lib="webworker" />
-
-declare const self: DedicatedWorkerGlobalScope;
-
-type Callback = typeof import('./arcaneChessInstance').callback;
-
-import { arcane } from 'src/arcaneChess/arcaneChessInstance';
-
-export const arcaneChessRPC = () => arcane();
